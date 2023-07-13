d0bb2f008488875d191769801658e9ace19d0050
Пирожков Виталий Сергеевич <pirozhkov.vs@prg-.it.ru>
2023-07-13 17:16:35 +0500

    init

---
+++ TestProjectServer/Controllers/Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestProject.Models;
+
+namespace TestProject.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class Controller : ControllerBase
+    {
+        [HttpPost]
+        public double GetPlan([FromBody] KitchenParameters parameters)
+        {
+            return 1;
+        }
+    }
+}
---
+++ TestProjectServer/Models/KitchenParameters.cs
@@ -0,0 +1,16 @@
+﻿namespace TestProject.Models
+{
+    public class KitchenParameters
+    {
+        public float KitchenLength { get; set; }
+        public float KitchenWidth { get; set; }
+        public float PipeX { get; set; }
+        public float PipeY { get; set; }
+        public float SinkLength { get; set; }
+        public float SinkWidth { get; set; }
+        public float StoveLength { get; set; }
+        public float StoveWidth { get; set; }
+        public bool IsCornerStove { get; set; }
+        public bool IsCornerSink { get; set; }
+    }
+}
