f790d48b52205d7f2eaf79a120d75bb05325d8a5
unknown <pirozhkov.vitaly@gmail.com>
2024-09-08 11:46:16 +0500

    init commit

---
+++ Controllers/DataController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using TestProjectServer.Entities;
+
+namespace TestProjectServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DataController : ControllerBase
+    {
+        private readonly ILogger<DataController> _logger;
+        private readonly DataManager _dataManager;
+
+        public DataController(ILogger<DataController> logger)
+        {
+            _logger = logger;
+            _dataManager = new DataManager();
+        }
+
+        [HttpGet(Name = "GetData")]
+        public async Task<IActionResult> GetData()
+        {
+            try
+            {
+                _logger.LogInformation("Получение данных с сервера");
+                var data = await _dataManager.GetDataAsync();
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Произошла ошибка при получении данных");
+                return StatusCode(500, "Произошла ошибка на сервере");
+            }
+        }
+    }
+}
---
+++ Entities/DataManager.cs
@@ -0,0 +1,44 @@
+﻿namespace TestProjectServer.Entities
+{
+    public class DataManager
+    {
+        public async Task<string> GetDataAsync()
+        {
+            DateTime currentTime = DateTime.Now;
+            string dateNumbers = $"{currentTime:ddMMyyyyHHmmss}";
+
+            int evenCount = 0;
+            int oddCount = 0;
+
+            foreach (char c in dateNumbers)
+            {
+                int digit = (int)char.GetNumericValue(c);
+
+                if (digit % 2 == 0)
+                {
+                    evenCount++;
+                }
+                else
+                {
+                    oddCount++;
+                }
+            }
+
+            string result;
+            if (evenCount > oddCount)
+            {
+                result = "чет!";
+            }
+            else if (oddCount > evenCount)
+            {
+                result = "нечет!";
+            }
+            else
+            {
+                result = "равно!";
+            }
+
+            return await Task.FromResult(result);
+        }
+    }
+}
