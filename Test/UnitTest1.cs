46d17f079feb7ce8582849a0e0e78840ee779a45
VitalyPirozhkov <96567605+VitalyPirozhkov@users.noreply.github.com>
2024-09-10 12:42:51 +0500

    Update Tests

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -15,11 +15,34 @@ namespace IntgrationServerTest
         }
 
         [Fact]
-        public async Task GetData_ReturnsSuccessStatusCode()
+        public async Task GetData_ReturnsSuccessStatusCode_WhenCalled()
         {
             var response = await _client.GetAsync("/GetData");
-
             response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task GetData_ReturnsSuccessStatusCode_ForConcurrentRequests()
+        {
+            var tasks = new List<Task<HttpResponseMessage>>();
+            for (int i = 0; i < 100; i++)
+            {
+                tasks.Add(_client.GetAsync("/GetData"));
+            }
+
+            var responses = await Task.WhenAll(tasks);
+
+            foreach (var response in responses)
+            {
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            }
+        }
+
+        [Fact]
+        public async Task InvalidEndpoint_ReturnsNotFoundStatusCode_WhenCalled()
+        {
+            var response = await _client.GetAsync("/InvalidEndpoint");
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         }
     }
 }
